3bf045b64f0c3dce4c28f9d89d37272450ad309c
ifTNT <happyjohn369@gmail.com>
2018-06-13 02:50:30 +0800

    Fix fullscreen of mobile

--- js/future.js
+++ js/future.js
@@ -1,11 +1,16 @@
 $(document).ready(init);
 function init(){
-    $('body').bind('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta /120 > 0) {
-            console.log('scrolling up !');
-        }
-        else{
-            console.log('scrolling down !');
+    $(window).bind('mousewheel', function(e){
+        if(!($('body').data('scrolling') || false)){
+            $('body').data('scrolling', true);
+            if(e.originalEvent.wheelDelta /120 > 0) {
+                console.log('scrolling up !');
+            }
+            else{
+                console.log('scrolling down !');
+            }
+            $('#page1').slideToggle(1000);
+            setTimeout(()=>{$('body').data('scrolling', false)}, 400);
         }
     });
     $('.next-page').on('click',function(){
