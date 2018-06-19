fe9adc6aecd65c3fbfa2c92a2deee87f4dcd266e
ifTNT <happyjohn369@gmail.com>
2018-06-19 22:56:35 +0800

    Begin using vue

--- js/future/future.js
+++ js/future/future.js
@@ -1,3 +1,4 @@
+var game
 var pages = [
     {
         'sectionID': '#page1',
@@ -71,89 +72,94 @@ var pages = [
 
 $(document).ready(init);
 function init(){
-    $('body').data('scrolling', false);
-    $('body').data('block-next', false);
-    setTimeout(()=>{
+    game = new Vue({
+        el: '#game',
+        data:{
+            'scrolling':  false,
+            'blockNext': false,
+            'char': '',
+            'nowPage': 0,
+            'arrow': {
+                'landed': false,
+                'seen': true,
+                'isBlack': false,
+                'bouncing': true,
+                'css': {}
+            }
+        },
+        methods:{
+            'transitPage': function (){
+                var a = pages[this.nowPage];
+                var newP = a.nextPage();
+                var b = pages[newP];
+                $(b.sectionID).css("z-index", "-2");
+                $(b.sectionID).show();
+                $(a.sectionID).slideUp(1000)
+                
+                // Hide nav arrow on the last page
+                this.nowPage = newP;
+                if(b.nextPage==undefined){
+                    this.arrow.seen = false;
+                }
+                setTimeout(()=>$(b.sectionID).css("z-index", ""), 1000);
+            
+                if(a.DarkBg != b.DarkBg){
+                    this.arrow.isBlack = !this.arrow.isBlack;
+                }
+                b.enterPoint();
+                this.landArrow();
+            },
+            'landArrow': function(){
+                if(this.arrow.landed == true) return;
+                this.arrow.landed = true;
+                var nowPos = $('#nav-arrow').css("padding-top");
+                this.arrow.bouncing = false;
+                this.arrow.css['padding-top'] = nowPos;
+                this.$forceUpdate();
+                /* Transit using pure css */
+                setTimeout(()=>{
+                    this.arrow.css['padding-top']="10vh";
+                    this.$forceUpdate();
+                }, 100);
+                setTimeout(() => {
+                    this.arrow.css['transition'] = "color 2s";
+                    this.arrow.css['padding-top'] = "1em";
+                    this.arrow.css['height'] = "2em";
+                    this.$forceUpdate();
+                }, 1500);
+            },
+            'fullScreen': function() {
+                $('#goFS').fadeOut();
+                $('#page1 h1').text("DALABONBA I");
+                //$('#page1 h1').fadeOut(8000);
+                this.$forceUpdate();
+                toggleFullScreen();
+            }
+        },
+    });
+
     var mc = new Hammer($('body').get(0));
     mc.get('pan').set({ direction: Hammer.DIRECTION_UP });
     mc.on("panup", function(ev) {
-        if(!$('body').data('scrolling') && !$('body').data('block-next')){
-            $('body').data('scrolling', true);
-            transitPage(pages[$('body').data('nowPage')].nextPage());
+        if(!game.scrolling && !game.blockNext){
+            game.scrolling = true;
+            game.transitPage(pages[game.nowPage].nextPage());
             setTimeout(()=>{
-                $('body').data('scrolling', false);
+                game.scrolling = false;
             }, 1000);
         }
     });
-    },2000);
-    /*$(window).bind('scroll', function(e){
-        if(!$('body').data('scrolling') && !$('body').data('block-next')){
-            $('body').data('scrolling', true);
-            //setTimeout(()=>{
-                transitPage(pages[$('body').data('nowPage')].nextPage());
-            //}, 0);
-            setTimeout(()=>{
-                $('body').data('scrolling', false);
-            }, 1000);
-        }
-    });*/
-    $('.next-page').on('click',function(){
-        transitPage(pages[$('body').data('nowPage')].nextPage());
-    });
+
     var p = window.location.hash.substr(1) || 0;
-    $('body').data('nowPage', p);
+    game.nowPage = p;
     for(var i of pages){
         $(i.sectionID).hide();
     }
     $(pages[p].sectionID).show();
     if(pages[p].DarkBg != true){
-        $('#nav-arrow').addClass("black");
+        game.arrow.isBlack = true;
     }
     pages[p].enterPoint();
     setTimeout(()=>window.scrollTo(0,1),0);
-    if(p!=0) landArrow();
-
-    $('#goFS').on("click", function() {
-        $('#goFS').fadeOut();
-        $('#page1 h1').text("DALABONBA I");
-        //$('#page1 h1').fadeOut(8000);
-        toggleFullScreen();
-    });
-}
-function transitPage(newP){
-    var a = pages[$('body').data('nowPage')];
-    var b = pages[newP];
-    $(b.sectionID).css("z-index", "-2");
-    $(b.sectionID).show();
-    $(a.sectionID).slideUp(1000)
-    
-    // Hide nav arrow on the last page
-    var p = $('body').data('nowPage');
-    $('body').data('nowPage', newP);
-    if(b.nextPage==undefined){
-        $('#nav-arrow').hide();
-    }
-    setTimeout(()=>$(b.sectionID).css("z-index", ""), 1000);
-
-    if(a.DarkBg != b.DarkBg){
-        $('#nav-arrow').toggleClass("black");
-    }
-    b.enterPoint();
-    landArrow();
-}
-
-function landArrow(){
-    if($('#nav-arrow').data("landed") == true) return;
-    $('#nav-arrow').data("landed", true);
-    var nowPos = $('#nav-arrow').css("padding-top");
-    //console.log(nowPos);
-    $('#nav-arrow').removeClass("bounce");
-    $('#nav-arrow').css("padding-top", nowPos);
-    /* Transit using pure css */
-    setTimeout(()=>$('#nav-arrow').css("padding-top", "10vh"), 1);
-    setTimeout(() => {
-        $('#nav-arrow').css("transition", "color 2s");
-        $('#nav-arrow').css("padding-top", "1em");
-        $('#nav-arrow').css("height", "2em");
-    }, 1500);
+    if(p!=0) game.landArrow();
 }
